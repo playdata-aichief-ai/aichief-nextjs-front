77a965598466fefb47c87f8872869eae68d25a2b
ZoMD <omomo010@gmail.com>
2022-11-10 23:18:21 +0900

    add: http-proxy-middleware 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/claim',
+    '/',
     createProxyMiddleware({
       target: process.env.NEXT_PUBLIC_BACKEND_URL,
       changeOrigin: true,
